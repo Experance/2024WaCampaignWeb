2252fe657b85c160560466ad19fcc36b22cf8b00
Andrew Wood <andrewrxzwood@gmail.com>
2024-08-01 00:28:07 -0700

    added async (better viewing)

--- scripts.js
+++ scripts.js
@@ -11,14 +11,22 @@ let pdfDoc = null,
 // Asynchronously download PDF
 getDocument(url).promise.then((pdfDoc_) => {
     pdfDoc = pdfDoc_;
-    console.log("reached")
-    renderPages();
+    
+    renderPages().then(() => {
+        // Doing things after pdf loads
+        document.getElementById("hyperlink").classList.remove('hidden'); // Show the hyperlink after the PDF loads
+        document.body.style.backgroundColor = "#75b8a7"; 
+    }).catch((error) => {
+        console.error(`Could not get products: ${error}`);
+    });
 });
 
-function renderPages() {
+async function renderPages() {
     for (let num = 1; num <= pdfDoc.numPages; num++) {
         renderPage(num);
-    }
+    } 
+    
+    
 }
 
 function renderPage(num) {
@@ -38,3 +46,5 @@ function renderPage(num) {
         page.render(renderContext);
     });
 }
+
+
