cdfc399b239814d3f289e4c4e614bdf9fb94d6e2
Andrew Wood <andrewrxzwood@gmail.com>
2024-08-01 21:06:21 -0700

    images working

--- scripts.js
+++ scripts.js
@@ -1,51 +1,7 @@
-import { getDocument, GlobalWorkerOptions } from './build/pdf.mjs';
+document.getElementById("note-container").classList.remove("hidden");
 
-const url = 'PastorsPicksWA.pdf';  // URL to your PDF file
 
-GlobalWorkerOptions.workerSrc = './build/pdf.worker.mjs';
 
-let pdfDoc = null,
-    scale = 8,
-    viewer = document.getElementById('pdf-canvas');
-
-// Asynchronously download PDF
-getDocument(url).promise.then((pdfDoc_) => {
-    pdfDoc = pdfDoc_;
-    
-    renderPages().then(() => {
-        // Doing things after pdf loads
-        
-        document.getElementById("note-container").classList.remove('hidden'); // Show the note after the PDF loads
-        // document.body.style.backgroundColor = "#75b8a7"; 
-    }).catch((error) => {
-        console.error(`Could not get products: ${error}`);
-    });
-});
-
-async function renderPages() {
-    for (let num = 1; num <= pdfDoc.numPages; num++) {
-        renderPage(num);
-    } 
-}
-
-function renderPage(num) {
-    pdfDoc.getPage(num).then((page) => {
-        const viewport = page.getViewport({ scale: scale });
-        const canvas = document.createElement('canvas');
-        const ctx = canvas.getContext('2d');
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
-        canvas.style.width = "100%";
-
-        const renderContext = {
-            canvasContext: ctx,
-            viewport: viewport
-        };
-
-        viewer.appendChild(canvas);
-        page.render(renderContext);
-    });
-}
  /*
   Scrolling Event Listener for arrow 
 document.addEventListener('scroll', () => {
